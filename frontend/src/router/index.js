4e40a05a88dddb2d7bc4caa0ee2ae35a0910b990
nobata <ahirururu122@gmail.com>
2023-08-07 11:34:17 +0900

    トップページのデータと詳細ページのデータを取得

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Home from "../views/Home.vue";
+import Job from "../views/Job.vue";
 
 Vue.use(VueRouter);
 
@@ -8,22 +9,18 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: Home,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/job/:id",
+    name: "job",
+    component: Job,
+    props: true,
   },
 ];
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
   routes,
 });
 
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <v-container>
+      <div v-for="job in jobs" :key="job.pk">
+        <h2>
+          <router-link
+            :to="{ name: 'job', params: { id: job.id } }"
+            class="job-link"
+            >{{ job.company_name }}
+          </router-link>
+        </h2>
+        <p>{{ job.job_title }}</p>
+        <hr />
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import { apiService } from "../common/api.service.js";
+export default {
+  name: "homeView",
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  methods: {
+    getJobs() {
+      let endpoint = "api/jobs/";
+      apiService(endpoint).then((data) => {
+        this.jobs.push(...data.results);
+      });
+    },
+  },
+  created() {
+    this.getJobs();
+    document.title = "Job Board";
+  },
+};
+</script>
+
+<style scoped>
+.job-link {
+  font-weight: bold;
+  color: black;
+  text-decoration: none;
+}
+
+.job-link:hover {
+  color: #41b883;
+}
+</style>
