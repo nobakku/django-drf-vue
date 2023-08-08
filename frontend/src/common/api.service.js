ea9fd7d211e51857b3758eecb1d791e048a6a718
nobata <ahirururu122@gmail.com>
2023-08-09 08:54:39 +0900

    api.service.jsとcsrf_token.jsにコメント追加

--- frontend/src/common/api.service.js
+++ frontend/src/common/api.service.js
@@ -1,25 +1,38 @@
 import { CSRF_TOKEN } from "./csrf_token.js";
 
+// レスポンスを処理する関数
 function handleResponse(response) {
+  // ステータスコードが204の場合、空文字列を返す
   if (response.status === 204) {
     return "";
-  } else if (response.status === 404) {
+  }
+  // ステータスコードが404の場合、nullを返す
+  else if (response.status === 404) {
     return null;
-  } else {
+  }
+  // それ以外の場合、レスポンスをJSON形式で返す
+  else {
     return response.json();
   }
 }
 
+// APIサービスを提供する関数
 function apiService(endpoint, method, data) {
+  // 設定オブジェクトを作成
   const config = {
+    // メソッドを指定（デフォルトはGET）
     method: method || "GET",
+    // データがある場合はJSON形式に変換して設定（ない場合はnull）
     body: data !== undefined ? JSON.stringify(data) : null,
+    // ヘッダーを設定
     headers: {
       "content-type": "application/json",
       "X-CSRFTOKEN": CSRF_TOKEN,
     },
   };
+  // エンドポイントと設定を元にフェッチリクエストを行い、レスポンスを処理する
   return fetch(endpoint, config).then(handleResponse);
 }
 
+// apiService関数を他のファイルから利用できるようにエクスポートする
 export { apiService };
--- frontend/src/common/csrf_token.js
+++ frontend/src/common/csrf_token.js
@@ -1,17 +1,32 @@
+// 特定の名前のクッキーの値を取得する関数
 function getCookie(name) {
   var cookieValue = null;
+
+  // ドキュメントのクッキーコンテンツが存在し、空でない場合
   if (document.cookie && document.cookie !== "") {
+    // クッキーをセミコロンで分割し、配列に格納する
     var cookies = document.cookie.split(";");
+
+    // クッキーの数だけ繰り返すループ
     for (var i = 0; i < cookies.length; i++) {
+      // クッキーの前後の余分なスペースをトリムする
       var cookie = cookies[i].trim();
+
+      // クッキーが指定した名前と等しい場合
       if (cookie.substring(0, name.length + 1) === name + "=") {
+        // クッキーの値をデコードし、cookieValueに代入する
         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
         break;
       }
     }
   }
+
+  // 取得したクッキーの値を返す
   return cookieValue;
 }
+
+// "csrftoken"という名前のクッキーの値を取得し、CSRF_TOKENに代入する
 var CSRF_TOKEN = getCookie("csrftoken");
 
+// CSRF_TOKENを他のファイルから利用できるようにエクスポートする
 export { CSRF_TOKEN };
